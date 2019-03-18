56e04edfc72f5b3264bb3289e0828841aad6547b
Adam Piziak <adpiziak@gmail.com>
2019-03-18 14:27:55 -0400

    intialize git in travis

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,11 +4,12 @@ if [ $TRAVIS_BRANCH == "master" ] ; then
     eval "$(ssh-agent -s)"
     ssh-add ~/.ssh/travis_rsa
 
-    git remote add deploy "travis@scarletbus.com:/ScarletBus/api/.git/"
+    git init
+    git remote add deploy "travis@scarletbus.com:/ScarletBus/api/"
     git config user.name "Travis CI"
     git config user.email "travis@scarletbus.com"
 
     git add .
     git commit -m "Deploy"
-    git push -f deploy HEAD:master
+    git push -f deploy master
 fi
