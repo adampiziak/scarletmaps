da43108a21e3106507d5022bdbafee171526772c
Adam Piziak <adpiziak@gmail.com>
2019-03-18 12:25:58 -0400

    trying again

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 if [ $TRAVIS_BRANCH == "master" ] ; then
-
     eval "$(ssh-agent -s)"
     ssh-add ~/.ssh/travis_rsa
+
     git remote add deploy "travis@scarletbus.com:/ScarletBus/api/"
     git config user.name "Travis CI"
     git config user.email "travis@scarletbus.com"
 
     git add .
-    git status # debug
-    git commit -m "deploy"
+    git commit -m "Deploy"
     git push -f deploy HEAD:master
 fi
