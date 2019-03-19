6b1ce62b5880df2c08775b8693b88c0997f71b94
Adam Piziak <adpiziak@gmail.com>
2019-03-18 20:56:58 -0400

    travis test

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,7 @@ type TranslocSchema = RootNode<'static, TranslocDatabase, EmptyMutation<Transloc
 
 #[get("/")]
 fn root() -> &'static str {
-    "Hello there"
+    "Hello therea"
 }
 
 #[get("/graphql")]
